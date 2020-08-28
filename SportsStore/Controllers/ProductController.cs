b6334ea027e04ec37421ab25151d033347490ee1
tek-gal <tek-gal@yandex.com>
2020-08-28 23:23:13 +0300

    add structure and test directory

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers {
+
+    public class ProductController : Controller {
+
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository repository) {
+            this.repository = repository;
+        }
+
+        public ViewResult List() => View(repository.Products);
+
+    }
+
+}
---
+++ SportsStore/Models/IProductRepository.cs
@@ -0,0 +1,11 @@
+using System.Linq;
+
+namespace SportsStore.Models {
+
+    public interface IProductRepository {
+        
+        IQueryable<Product> Products { get; }
+
+    }
+
+}
